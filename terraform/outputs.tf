db627439c972d5ad643090a2dd14cb237b6652c3
andharaester15 <andharaester@mhs.eng.upr.ac.id>
2026-05-12 18:00:00 +0700

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,6 @@
-output "server1_public_ip" {
-  value = aws_instance.server1.public_ip
+output "vpc_id" {
+value = aws_vpc.tf_vpc.id
 }
 
-output "server2_private_ip" {
-  value = aws_instance.server2.private_ip
+output "public_subnet_id" { value = aws_subnet.public.id
 }
