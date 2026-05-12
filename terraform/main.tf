fe31f7adce0268f95436869564e5727a7b7b9359
andharaester15 <andharaester@mhs.eng.upr.ac.id>
2026-05-12 17:59:36 +0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,204 +1,58 @@
 provider "aws" {
-  region = "ap-southeast-1"
+region = "ap-southeast-1"
 }
 
-# =====================================
-# S3 Bucket (Frontend Website)
-# =====================================
-resource "aws_s3_bucket" "frontend" {
-  bucket = "url-shortener-storage-921599273919-ap-southeast-1-an"
+# ====================== # VPC
+# ======================
+resource "aws_vpc" "tf_vpc" { cidr_block	= "10.1.0.0/16" enable_dns_hostnames = true enable_dns_support = true
 
-  tags = {
-    Name        = "URL Shortener Frontend"
-    Environment = "Production"
-  }
+tags = {
+Name = "vpc-url-shortener-terraform"
+}
 }
 
-resource "aws_s3_bucket_website_configuration" "frontend_website" {
-  bucket = aws_s3_bucket.frontend.id
+# ======================
+# Subnet Public
+# ======================
+resource "aws_subnet" "public" { vpc_id	= aws_vpc.tf_vpc.id
+cidr_block	= "10.1.1.0/24" map_public_ip_on_launch = true
 
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "index.html"
-  }
+tags = {
+Name = "subnet-public-terraform"
+}
 }
 
-resource "aws_s3_bucket_public_access_block" "frontend_access" {
-  bucket = aws_s3_bucket.frontend.id
+# ======================
+# Subnet Private
+# ======================
+resource "aws_subnet" "private" { vpc_id	= aws_vpc.tf_vpc.id cidr_block = "10.1.2.0/24"
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+tags = {
+Name = "subnet-private-terraform"
+}
 }
 
-# =====================================
-# DynamoDB Table
-# =====================================
-resource "aws_dynamodb_table" "url_mapping" {
-  name         = "url-mapping"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "id"
-
-  attribute {
-    name = "id"
-    type = "S"
-  }
-
-  tags = {
-    Name = "URL Mapping Table"
-  }
+# ======================
+resource "aws_internet_gateway" "igw" { vpc_id = aws_vpc.tf_vpc.id
+tags = {
+Name = "igw-terraform"
+}
+}
+# ======================
+# Route Table
+# ======================
+resource "aws_route_table" "public_rt" { vpc_id = aws_vpc.tf_vpc.id
+route {
+cidr_block = "0.0.0.0/0"
+gateway_id = aws_internet_gateway.igw.id
+}
+tags = {
+Name = "rt-public-terraform"
+}
+}
+# ====================== # Associate RT ke Subnet Public # ======================
+resource "aws_route_table_association" "assoc" { subnet_id	= aws_subnet.public.id route_table_id = aws_route_table.public_rt.id
 }
 
-# =====================================
-# IAM Role for Lambda
-# =====================================
-resource "aws_iam_role" "lambda_role" {
-  name = "url-shortener-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      },
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "basic_execution" {
-  role       = aws_iam_role.lambda_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-resource "aws_iam_role_policy_attachment" "dynamodb_access" {
-  role       = aws_iam_role.lambda_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
-}
-
-# =====================================
-# Lambda Function
-# =====================================
-resource "aws_lambda_function" "backend" {
-  function_name = "url-shortener"
-  role          = aws_iam_role.lambda_role.arn
-  runtime       = "nodejs20.x"
-  handler       = "index.handler"
-
-  filename         = "../app/backend/url-shortener.zip"
-  source_code_hash = filebase64sha256("../app/backend/url-shortener.zip")
-
-  timeout = 10
-
-  environment {
-    variables = {
-      TABLE_NAME = aws_dynamodb_table.url_mapping.name
-    }
-  }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.basic_execution,
-    aws_iam_role_policy_attachment.dynamodb_access
-  ]
-}
-
-# =====================================
-# API Gateway HTTP API
-# =====================================
-resource "aws_apigatewayv2_api" "api" {
-  name          = "url-shortener-api"
-  protocol_type = "HTTP"
-}
-
-resource "aws_apigatewayv2_integration" "lambda_integration" {
-  api_id                 = aws_apigatewayv2_api.api.id
-  integration_type       = "AWS_PROXY"
-  integration_uri        = aws_lambda_function.backend.invoke_arn
-  payload_format_version = "2.0"
-}
-
-resource "aws_apigatewayv2_route" "create_short_url" {
-  api_id    = aws_apigatewayv2_api.api.id
-  route_key = "POST /shorten"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
-}
-
-resource "aws_apigatewayv2_route" "redirect_url" {
-  api_id    = aws_apigatewayv2_api.api.id
-  route_key = "GET /{id}"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
-}
-
-resource "aws_apigatewayv2_stage" "default" {
-  api_id      = aws_apigatewayv2_api.api.id
-  name        = "$default"
-  auto_deploy = true
-}
-
-resource "aws_lambda_permission" "api_permission" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.backend.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
-}
-
-# =====================================
-# CloudFront Distribution
-# =====================================
-resource "aws_cloudfront_distribution" "cdn" {
-  enabled = true
-
-  origin {
-    domain_name = aws_s3_bucket.frontend.bucket_regional_domain_name
-    origin_id   = "s3-frontend"
-  }
-
-  default_cache_behavior {
-    target_origin_id       = "s3-frontend"
-    viewer_protocol_policy = "redirect-to-https"
-
-    allowed_methods = ["GET", "HEAD"]
-    cached_methods  = ["GET", "HEAD"]
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-  }
-
-  default_root_object = "index.html"
-
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
-
-  viewer_certificate {
-    cloudfront_default_certificate = true
-  }
-}
-
-# =====================================
-# Outputs
-# =====================================
-output "website_url" {
-  value = aws_s3_bucket_website_configuration.frontend_website.website_endpoint
-}
-
-output "api_url" {
-  value = aws_apigatewayv2_api.api.api_endpoint
-}
-
-output "cloudfront_url" {
-  value = aws_cloudfront_distribution.cdn.domain_name
-}
+# Internet Gateway
+# ======================
